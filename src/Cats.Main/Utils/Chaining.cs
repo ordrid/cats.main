16ef930c41713cabe99d16a5e59572905d6daac8
Michael B. Espeña <espena.michael.pub@gmail.com>
2024-03-06 14:32:21 +0800

    add XML documentation to utility functions

--- src/Cats.Main/Utils/Chaining.cs
+++ src/Cats.Main/Utils/Chaining.cs
@@ -6,10 +6,33 @@ using Cats.Main.Core;
 
 namespace Cats.Main.Utils;
 
+/// <summary>
+/// Provides extension methods for functional chaining operations.
+/// </summary>
 public static class Chaining
 {
+    /// <summary>
+    /// Maps a value of type <typeparamref name="TIn"/> to a value of type <typeparamref name="TOut"/> using the provided function.
+    /// </summary>
+    /// <typeparam name="TIn">The input type.</typeparam>
+    /// <typeparam name="TOut">The output type.</typeparam>
+    /// <param name="t">The input value.</param>
+    /// <param name="f">The function to apply.</param>
+    /// <returns>The result of applying the function to the input value.</returns>
     public static TOut Map<TIn, TOut>(this TIn t, Func<TIn, TOut> f) => f(t);
 
+    /// <summary>
+    /// Applies two functions to the input value and combines their results using the provided function.
+    /// </summary>
+    /// <typeparam name="TIn">The input type.</typeparam>
+    /// <typeparam name="T1">The type of the first function result.</typeparam>
+    /// <typeparam name="T2">The type of the second function result.</typeparam>
+    /// <typeparam name="TOut">The output type.</typeparam>
+    /// <param name="t">The input value.</param>
+    /// <param name="f1">The first function to apply.</param>
+    /// <param name="f2">The second function to apply.</param>
+    /// <param name="fout">The function to combine the results of the first two functions.</param>
+    /// <returns>The result of combining the results of the two functions.</returns>
     public static TOut Fork<TIn, T1, T2, TOut>(
         this TIn t,
         Func<TIn, T1> f1,
@@ -17,6 +40,16 @@ public static class Chaining
         Func<T1, T2, TOut> fout
     ) => fout(f1(t), f2(t));
 
+    /// <summary>
+    /// Applies multiple functions to the input value and combines their results using the provided function.
+    /// </summary>
+    /// <typeparam name="TStart">The input type.</typeparam>
+    /// <typeparam name="TMiddle">The intermediate type.</typeparam>
+    /// <typeparam name="TEnd">The output type.</typeparam>
+    /// <param name="t">The input value.</param>
+    /// <param name="joinFunc">The function to combine the results of the prong functions.</param>
+    /// <param name="prongs">The prong functions to apply.</param>
+    /// <returns>The result of combining the results of the prong functions.</returns>
     public static TEnd Fork<TStart, TMiddle, TEnd>(
         this TStart t,
         Func<TMiddle, TEnd> joinFunc,
@@ -28,31 +61,84 @@ public static class Chaining
         return returnValue;
     }
 
+    /// <summary>
+    /// Selects the first non-null result from a sequence of functions applied to the input value.
+    /// </summary>
+    /// <typeparam name="TIn">The input type.</typeparam>
+    /// <typeparam name="TOut">The output type.</typeparam>
+    /// <param name="t">The input value.</param>
+    /// <param name="args">The functions to apply.</param>
+    /// <returns>The result of the first non-null function application.</returns>
     public static TOut Alt<TIn, TOut>(this TIn t, params Func<TIn, TOut>[] args) =>
         args.Select(x => x(t)).First(x => x is not null);
 
+    /// <summary>
+    /// Composes two functions together.
+    /// </summary>
+    /// <typeparam name="TIn">The input type.</typeparam>
+    /// <typeparam name="TOutOld">The output type of the first function.</typeparam>
+    /// <typeparam name="TOutNew">The output type of the composed function.</typeparam>
+    /// <param name="t">The first function.</param>
+    /// <param name="f">The second function.</param>
+    /// <returns>A composed function.</returns>
     public static Func<TIn, TOutNew> Compose<TIn, TOutOld, TOutNew>(
         this Func<TIn, TOutOld> t,
         Func<TOutOld, TOutNew> f
     ) => x => f(t(x));
 
+    /// <summary>
+    /// Transforms and aggregates a sequence of input values.
+    /// </summary>
+    /// <typeparam name="TIn">The input type.</typeparam>
+    /// <typeparam name="TFilterOut">The filtered output type.</typeparam>
+    /// <typeparam name="TFinalOut">The final output type.</typeparam>
+    /// <param name="t">The input sequence.</param>
+    /// <param name="transformer">The transformation function.</param>
+    /// <param name="aggregator">The aggregation function.</param>
+    /// <returns>The aggregated result of applying the transformation function.</returns>
     public static TFinalOut Transduce<TIn, TFilterOut, TFinalOut>(
         this IEnumerable<TIn> t,
         Func<IEnumerable<TIn>, IEnumerable<TFilterOut>> transformer,
         Func<IEnumerable<TFilterOut>, TFinalOut> aggregator
     ) => aggregator(transformer(t));
 
+    /// <summary>
+    /// Converts a transformation function into a transducer function.
+    /// </summary>
+    /// <typeparam name="TIn">The input type.</typeparam>
+    /// <typeparam name="TO1">The intermediate type.</typeparam>
+    /// <typeparam name="TO2">The output type.</typeparam>
+    /// <param name="t">The transformation function.</param>
+    /// <param name="aggregator">The aggregation function.</param>
+    /// <returns>The transducer function.</returns>
     public static Func<IEnumerable<TIn>, TO2> ToTransducer<TIn, TO1, TO2>(
         this Func<IEnumerable<TIn>, IEnumerable<TO1>> t,
         Func<IEnumerable<TO1>, TO2> aggregator
     ) => x => aggregator(t(x));
 
+#pragma warning disable SA1625
+    /// <summary>
+    /// Executes an action on the input value and returns the value.
+    /// </summary>
+    /// <typeparam name="T">The input type.</typeparam>
+    /// <param name="t">The input value.</param>
+    /// <param name="action">The action to execute.</param>
+    /// <returns>The input value.</returns>
     public static T Tap<T>(this T t, Action<T> action)
     {
         action(t);
         return t;
     }
+#pragma warning restore SA1625
 
+    /// <summary>
+    /// Binds an <see cref="Option{TIn}"/> to a function.
+    /// </summary>
+    /// <typeparam name="TIn">The input type of the option.</typeparam>
+    /// <typeparam name="TOut">The output type.</typeparam>
+    /// <param name="t">The input option.</param>
+    /// <param name="f">The function to bind.</param>
+    /// <returns>An <see cref="Option{TOut}"/> resulting from the bind operation.</returns>
     public static Option<TOut> Bind<TIn, TOut>(this Option<TIn> t, Func<TIn, TOut> f)
     {
         return t switch
--- src/Cats.Main/Utils/Unwrapping.cs
+++ src/Cats.Main/Utils/Unwrapping.cs
@@ -6,8 +6,17 @@ using Cats.Main.Core;
 
 namespace Cats.Main.Utils;
 
+/// <summary>
+/// Provides extension methods for unwrapping values from <see cref="Try{T}"/> and <see cref="Option{T}"/>.
+/// </summary>
 public static class Unwrapping
 {
+    /// <summary>
+    /// Unwraps the value from the <see cref="Try{T}"/> instance.
+    /// </summary>
+    /// <typeparam name="T">The type of the value contained in the <see cref="Try{T}"/>.</typeparam>
+    /// <param name="result">The <see cref="Try{T}"/> instance.</param>
+    /// <returns>The unwrapped value if the <see cref="Try{T}"/> represents a success; otherwise, throws <see cref="NotSupportedException"/>.</returns>
     public static T Unwrap<T>(this Try<T> result) =>
         result switch
         {
@@ -15,6 +24,12 @@ public static class Unwrapping
             _ => throw new NotSupportedException("Cannot unwrap the value of an error.")
         };
 
+    /// <summary>
+    /// Unwraps the value from the <see cref="Option{T}"/> instance.
+    /// </summary>
+    /// <typeparam name="T">The type of the value contained in the <see cref="Option{T}"/>.</typeparam>
+    /// <param name="option">The <see cref="Option{T}"/> instance.</param>
+    /// <returns>The unwrapped value if the <see cref="Option{T}"/> is Some; otherwise, throws <see cref="NotSupportedException"/>.</returns>
     public static T Unwrap<T>(this Option<T> option) =>
         option switch
         {
@@ -22,6 +37,13 @@ public static class Unwrapping
             _ => throw new NotSupportedException("Cannot unwrap the value of None.")
         };
 
+    /// <summary>
+    /// Unwraps the value from the <see cref="Try{T}"/> instance or returns an alternative value.
+    /// </summary>
+    /// <typeparam name="T">The type of the value contained in the <see cref="Try{T}"/>.</typeparam>
+    /// <param name="result">The <see cref="Try{T}"/> instance.</param>
+    /// <param name="alternative">The alternative value to return if the <see cref="Try{T}"/> represents an error.</param>
+    /// <returns>The unwrapped value if the <see cref="Try{T}"/> represents a success; otherwise, returns the alternative value.</returns>
     public static T UnwrapOrElse<T>(this Try<T> result, T alternative) =>
         result switch
         {
@@ -29,6 +51,13 @@ public static class Unwrapping
             _ => alternative,
         };
 
+    /// <summary>
+    /// Unwraps the value from the <see cref="Option{T}"/> instance or returns an alternative value.
+    /// </summary>
+    /// <typeparam name="T">The type of the value contained in the <see cref="Option{T}"/>.</typeparam>
+    /// <param name="option">The <see cref="Option{T}"/> instance.</param>
+    /// <param name="alternative">The alternative value to return if the <see cref="Option{T}"/> is None.</param>
+    /// <returns>The unwrapped value if the <see cref="Option{T}"/> is Some; otherwise, returns the alternative value.</returns>
     public static T UnwrapOrElse<T>(this Option<T> option, T alternative) =>
         option switch
         {
