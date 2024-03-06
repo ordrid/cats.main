a7c15f10358c9f32fe2a42a577d6cbf311414d96
Michael B. Espeña <espena.michael.pub@gmail.com>
2024-03-06 14:31:58 +0800

    refactor `Error` class implementation

--- src/Cats.Main/Core/Error.cs
+++ src/Cats.Main/Core/Error.cs
@@ -7,9 +7,42 @@ namespace Cats.Main.Core;
 /// <summary>
 /// Represents an error.
 /// </summary>
-public abstract record Error(int Code, string Message);
+public abstract record Error
+{
+    /// <summary>
+    /// Many errors code.
+    /// </summary>
+    public const int ManyErrorsCode = -2_000_000_006;
+
+    /// <summary>
+    /// Gets the error code.
+    /// </summary>
+    public virtual int Code => 0;
+
+    /// <summary>
+    /// Gets the error message.
+    /// </summary>
+    public abstract string Message { get; }
+}
 
 /// <summary>
 /// Represents an due to an exception.
 /// </summary>
-public record Exceptional(int Code, Exception Exception) : Error(Code, Exception.Message);
+public record Exceptional(Exception Exception) : Error
+{
+    /// <inheritdoc/>
+    public override string Message => Exception.Message;
+}
+
+/// <summary>
+/// A list of errors.
+/// </summary>
+/// <param name="Errors"></param>
+public record ManyErrors(List<Error> Errors) : Error
+{
+    /// <inheritdoc/>
+    public override int Code => Error.ManyErrorsCode;
+
+    /// <inheritdoc/>
+    public override string Message => string.Join(", ", Errors);
+}
